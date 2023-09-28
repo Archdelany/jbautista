c8a7e577a5e818ebfb461daebfbcd5df7662d7ed
Carlos Bautista <88100281+Archdelany@users.noreply.github.com>
2023-09-28 10:19:25 -0600

    config base vite

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,11 @@
 
 export default {
+ bse:'/<REPO>/',
  root:'src',
   build: {
     outDir: '../dist',
     
   },
-
-  rollupOptions: {
-    input: {
-      main: resolve(__dirname, '/dist/index.html')
-    },
-  },
-
   server: {
     port: 8080
   }
