ae25b919d6624d2622aa4d66b1ba6f922b9e48d1
Carlos Bautista <88100281+Archdelany@users.noreply.github.com>
2023-09-28 01:59:58 -0600

    finalize test

--- src/js/home.js
+++ src/js/home.js
@@ -24,10 +24,14 @@ const cities = [
 packageType.addEventListener('change',()=>{
   if(packageType.value == 'hotel+flight' && document.getElementById('city') == null ){
     const container = document.createElement("div");
+    const label = document.createElement("label");
     const citieSelect = document.createElement("select");
     var carrouselWrapper = document.getElementById('w-carrousel');
     
     container.classList.add("mb-3","mb-xxl-0","me-md-2");
+    container.id='cityContainer';
+    const strongElement = document.createElement("strong");
+    strongElement.textContent = "City";
     citieSelect.name = "city";
     citieSelect.id ="city";
     citieSelect.classList.add("form-select","py-2");
@@ -38,27 +42,29 @@ packageType.addEventListener('change',()=>{
         option.text = city;
         citieSelect.appendChild(option);
     };
+    label.appendChild(strongElement);
+    container.appendChild(label);
     container.appendChild(citieSelect);
     form.insertBefore(container,checkIn);
 
     if(window.innerWidth < 1400){
 
-      gsap.to(carrouselWrapper,{marginTop:430});
+      gsap.to(carrouselWrapper,{marginTop:450});
 
     };
     
      
   }else{
 
-   if(document.getElementById('city')){
+   if(document.getElementById('cityContainer')){
 
     var carrouselWrapper = document.getElementById('w-carrousel');
 
-      document.getElementById('city').remove();
+      document.getElementById('cityContainer').remove();
 
       if(window.innerWidth < 1400){
 
-        gsap.to(carrouselWrapper,{marginTop:380});
+        gsap.to(carrouselWrapper,{marginTop:400});
         
       };
 
@@ -70,6 +76,7 @@ packageType.addEventListener('change',()=>{
   }
 
 });
+
 
 const checkIndate = document.getElementById('checkIn');
 const checkIndatepicker = new Datepicker(checkIndate, {
@@ -84,6 +91,56 @@ const checkOutDatepicker = new Datepicker(checkOutDate, {
 
 checkIndatepicker.setDate('today');
 checkOutDatepicker.setDate('today');
+
+/*--booking in modal--*/
+
+const packageTypeModal = document.getElementById('package-type-modal');
+const formModal = document.getElementById('booking-form-modal');
+const checkInModal = document.getElementById('checkInContainerModal');
+
+packageTypeModal.addEventListener('change',()=>{
+  if(packageTypeModal.value == 'hotel+flight' && document.getElementById('cityContainerModal') == null ){
+    const containerModal = document.createElement("div");
+    const citieSelectModal = document.createElement("select");
+    const labelModal = document.createElement("label");
+    const strongElementModal = document.createElement("strong");
+    strongElementModal.textContent = "City";
+    containerModal.classList.add("mb-3","mb-xxl-0","me-md-2");
+    containerModal.id='cityContainerModal';
+    citieSelectModal.name = "city-modal";
+    citieSelectModal.id ="city-modal";
+    citieSelectModal.classList.add("form-select","py-2");
+    for (const city of cities){
+
+        const optionModal = document.createElement("option");
+        optionModal.value = city;
+        optionModal.text = city;
+        citieSelectModal.appendChild(optionModal);
+    };
+    labelModal.appendChild(strongElementModal);
+    containerModal.appendChild(labelModal);
+    containerModal.appendChild(citieSelectModal);
+    formModal.insertBefore(containerModal,checkInModal);
+
+   
+     
+  }else{
+
+   if(document.getElementById('cityContainerModal')){
+
+
+      document.getElementById('cityContainerModal').remove();
+
+      
+   }
+
+
+
+  }
+
+});
+
+
 
 
 
@@ -142,11 +199,11 @@ window.addEventListener('resize',()=>{
 
       if(document.getElementById('city')){
 
-        gsap.to(carrouselWrapper,{marginTop:430});
+        gsap.to(carrouselWrapper,{marginTop:450});
 
       }else{
 
-        gsap.to(carrouselWrapper,{marginTop:380});
+        gsap.to(carrouselWrapper,{marginTop:400});
       }
 
   };
